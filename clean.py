db72ff3e701a6b6c6b9b2160b337f2f6b96e910a
sree93hari <sree93hari@gmail.com>
2017-04-04 20:43:16 +0530

    text cleaner initial version

---
+++ clean.py
@@ -0,0 +1,13 @@
+from string import punctuation
+import re
+
+def clean(text):
+        text = ''.join([c for c in text if c not in punctuation])
+        text = re.sub(r"\'ve", " have ", text)
+        text = re.sub(r"can't", "cannot ", text)
+        text = re.sub(r"n't", " not ", text)
+        text = re.sub(r"i m", "i am", text)
+        text = re.sub(r"\'re", " are ", text)
+        text = re.sub(r"\'d", " would ", text)
+        text = re.sub(r"\'ll", " will ", text)
+        return text
